b767528123b21b260445e54f7d2ab65f400bc7ab
Maarten de Boer <info@maartendeboer.net>
2015-01-09 02:51:32 +0100

    Added desaturation effect
    Cleaned up and commented the code

--- extension.js
+++ extension.js
@@ -9,117 +9,150 @@ const Clutter = imports.gi.Clutter;
 const SHADE_IN_TIME = 0.1;
 const SHADE_OUT_TIME = 2;
 const SHADE_BRIGHTNESS = -0.3;
+const SHADE_DESATURATION = 0.2;
 
 let on_window_created;
 
 const WindowShader = new Lang.Class({
-    Name: 'WindowShader',
+	Name: 'WindowShader',
 
-    _init: function(actor) {
-        this._effect = new Clutter.BrightnessContrastEffect();
-        actor.add_effect(this._effect);
-        this.actor = actor;
-        this._enabled = true;
-        this._shadeLevel = 0.0;
-        this._effect.enabled = (this._shadeLevel > 0);
-    },
+	_init: function(actor) {
+		this._enabled = true;
+		this._shadeLevel = 0.0;
 
-    set shadeLevel(level) {
-        this._shadeLevel = level;
-        this._effect.set_brightness(level * SHADE_BRIGHTNESS);
-        this._effect.enabled = (this._shadeLevel > 0);
-    },
+		/* Brightness effect */
+		this._effect = new Clutter.BrightnessContrastEffect();
+		this._effect.enabled = (this._shadeLevel > 0);
+		actor.add_effect(this._effect);
 
-    get shadeLevel() {
-        return this._shadeLevel;
-    }
+		/* Desaturation effect */
+		this._desat_effect = new Clutter.DesaturateEffect({ factor: 0.0 });
+		this._desat_effect.enabled = (this._shadeLevel > 0);
+		actor.add_effect(this._desat_effect);
+
+		this.actor = actor;
+	},
+
+	set shadeLevel(level) {
+		this._shadeLevel = level;
+
+		/* Brightness effect */
+		this._effect.set_brightness(level * SHADE_BRIGHTNESS);
+		this._effect.enabled = (this._shadeLevel > 0);
+
+		/* Desaturation effect */
+		this._desat_effect.set_factor(level * SHADE_DESATURATION);
+		this._desat_effect.enabled = (this._shadeLevel > 0);
+	},
+
+	get shadeLevel() {
+		return this._shadeLevel;
+	}
 });
+
 function init() {
 }
 
 function enable() {
 
-    function use_shader(meta_win) {
-	if (!meta_win) {
-	    return false;
+	/* Don't use our shader on certain window types */
+	function use_shader(meta_win) {
+		if (!meta_win)
+			return false;
+
+		var type = meta_win.get_window_type()
+		return (type == Meta.WindowType.NORMAL ||
+			type == Meta.WindowType.DIALOG ||
+			type == Meta.WindowType.MODAL_DIALOG);
 	}
-	var type = meta_win.get_window_type()
-	return (type == Meta.WindowType.NORMAL ||
-		type == Meta.WindowType.DIALOG ||
-		type == Meta.WindowType.MODAL_DIALOG);
-    }
 
-    function verifyShader(wa) {
-        if (wa._inactive_shader)
-            return;
-        var meta_win = wa.get_meta_window();
-        if (!use_shader(meta_win)) {
-            return;
-        }
-        wa._inactive_shader = new WindowShader(wa);
-        if(!wa._inactive_shader)
-            return;
-        if (!meta_win.has_focus()) {
-            Tweener.addTween(wa._inactive_shader,
-                             { shadeLevel: 1.0,
-                               time: SHADE_OUT_TIME,
-                               transition: 'linear'
-                             });
-        }
-    }
+	/* Verify if window has our effect shader */
+	function verifyShader(wa) {
+		if (wa._inactive_shader)
+			return;
 
-    function focus(the_window) {
-        global.get_window_actors().forEach(function(wa) {
-            verifyShader(wa);
-            if (!wa._inactive_shader)
-                return;
-            if (the_window == wa.get_meta_window()) {
-                Tweener.addTween(wa._inactive_shader,
-                                 { shadeLevel: 0.0,
-                                   time: SHADE_IN_TIME,
-                                   transition: 'linear'
-                 });
-            } else if(wa._inactive_shader.shadeLevel < 1.0) {
-                Tweener.addTween(wa._inactive_shader,
-                                 { shadeLevel: 1.0,
-                                   time: SHADE_OUT_TIME,
-                                   transition: 'linear'
-                                 });
-            }
-        });
-    }
+		var meta_win = wa.get_meta_window();
+		if (!use_shader(meta_win))
+			return;
 
-    function window_created(__unused_display, the_window) {
-        if (use_shader(the_window)) {
-            the_window._shade_on_focus = the_window.connect('focus', focus);
-        }
-    }
-    on_window_created = global.display.connect('window-created', window_created);
+		/* Shader for inactive windows */
+		wa._inactive_shader = new WindowShader(wa);
+		if(!wa._inactive_shader)
+			return;
 
-    global.get_window_actors().forEach(function(wa) {
-        var meta_win = wa.get_meta_window();
-        if (!meta_win) {
-            return;
-        }
-        verifyShader(wa);
-        window_created(null, wa.get_meta_window());
-    });
+		if (!meta_win.has_focus()) {
+			Tweener.addTween(wa._inactive_shader,
+				{ shadeLevel: 1.0,
+					time: SHADE_OUT_TIME,
+					transition: 'linear'
+				});
+		}
+	}
+
+	/* Toggle shader on focus */
+	function focus(the_window) {
+		global.get_window_actors().forEach(function(wa) {
+			
+			/* Verify our effect shader */
+			verifyShader(wa);
+			if (!wa._inactive_shader)
+				return;
+
+			/* Fade in/out our shader */
+			if (the_window == wa.get_meta_window()) {
+				Tweener.addTween(wa._inactive_shader,
+					{ shadeLevel: 0.0,
+						time: SHADE_IN_TIME,
+						transition: 'linear'
+					});
+			} else if(wa._inactive_shader.shadeLevel < 1.0) {
+				Tweener.addTween(wa._inactive_shader,
+					{ shadeLevel: 1.0,
+						time: SHADE_OUT_TIME,
+						transition: 'linear'
+					});
+			}
+		});
+	}
+
+	/* Catch new created windows and attach events */
+	function window_created(__unused_display, the_window) {
+		if (use_shader(the_window)) {
+			the_window._shade_on_focus = the_window.connect('focus', focus);
+		}
+	}
+	on_window_created = global.display.connect('window-created', window_created);
+
+	/* Loop existing windows, attach events and shaders */
+	global.get_window_actors().forEach(function(wa) {
+		var meta_win = wa.get_meta_window();
+		if (!meta_win)
+			return;
+
+		/* Verify our effect shader */
+		verifyShader(wa);
+		
+		window_created(null, wa.get_meta_window());
+	});
 }
 
 function disable() {
-    if (on_window_created) {
-        global.display.disconnect(on_window_created);
-    }
-    global.get_window_actors().forEach(function(wa) {
-        var win = wa.get_meta_window();
-        if (win && win._shade_on_focus) {
-            win.disconnect(win._shade_on_focus);
-            delete win._shade_on_focus;
-        }
-        if(wa._inactive_shader) {
-            wa._inactive_shader.shadeLevel = 0.0;
-            wa.remove_effect(wa._inactive_shader._effect);
-            delete wa._inactive_shader;
-        }
-    });
+	if (on_window_created) {
+		global.display.disconnect(on_window_created);
+	}
+
+	/* Loop existing windows, disconnect events and destroy shaders */
+	global.get_window_actors().forEach(function(wa) {
+		var win = wa.get_meta_window();
+		if (win && win._shade_on_focus) {
+			win.disconnect(win._shade_on_focus);
+			delete win._shade_on_focus;
+		}
+		if(wa._inactive_shader) {
+			wa._inactive_shader.shadeLevel = 0.0;
+			wa.remove_effect(wa._inactive_shader._effect);
+			wa.remove_effect(wa._inactive_shader._desat_effect);
+			delete wa._inactive_shader;
+		}
+	});
 }
